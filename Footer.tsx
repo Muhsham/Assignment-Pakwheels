c0cd8f8b930061564062c1ed7f465ad5c08d1ae2
Muhsham <160315548+Muhsham@users.noreply.github.com>
2024-10-27 21:06:22 +0500

    Add files via upload

---
+++ Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>&copy; Designed by Syed Haseeb Ali</p>
+      <p>***923332189915***</p>
+      <p>***923330214114***</p>
+    </footer>
+  );
+};
+
+export default Footer;
